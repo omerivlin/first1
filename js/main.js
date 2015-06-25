660586b23ecfaac10ad7225b7832e40da72550bd
omerivlin <omerivlin97@gmail.com>
2015-06-26 01:07:57 +0300

    wtf

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 var app = {
 	
+	findByName: function() {
+    var self = this;
+    this.store.findByName($('.search-key').val(), function(employees) {
+        $('.employee-list').html(self.employeeLiTpl(employees));
+    });
+	},
+	
+	renderHomeView: function() {
+    $('body').html(this.homeTpl());
+    $('.search-key').on('keyup', $.proxy(this.findByName, this));
+	},
 		
 	showAlert: function (message, title) {
     if (navigator.notification) {
@@ -12,11 +23,14 @@ var app = {
 	initialize: function() {
     var self = this;
     this.store = new MemoryStore(function() {
-        self.showAlert('Store Initialized', 'Info');
+		self.renderHomeView();
     });
-    $('.search-key').on('keyup', $.proxy(this.findByName, this));
+	this.homeTpl = Handlebars.compile($("#home-tpl").html());
+	this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
 	}
 	
 }; 
+
+
 
 app.initialize();
